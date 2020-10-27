bf0aac99639b15849ea63a6e3406f5186c3862d6
ArtemiusTheHuman <arthemius@list.ru>
2020-10-27 12:37:50 +0300

    Final version of this not-so-good algorithm.

--- src/main/java/Planner.java
+++ src/main/java/Planner.java
@@ -8,128 +8,130 @@ import java.util.*;
 public class Planner {
 
     public static void main(String[] args){
+        long startTime = System.currentTimeMillis();
         try{
             System.out.println("The Data file's name: "+args[0]);
-            long startTime = System.currentTimeMillis();
             planTasks(args[0]);
-            long endTime = System.currentTimeMillis();
-            System.out.println("Work is done.\nExecution time: "+((endTime-startTime)/1000)+"s");
-        } catch (Exception e) {
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Work is done.\nExecution time: "+((endTime-startTime)/1000)+"s");
     }
     /**
-     * Основной метод, делающий всю работу: читает файл, проверяет зависимости, выводит результат.
-     * @param fileName - имя файла на чтение, вносится аргументом при запуске
+     * Основной метод, делающий всю работу: читает файл, вспомогательным методом проверяет
+     * зависимости, выводит результат.
+     * @param fileName - имя файла на чтение, вносится аргументом при запуске в main
      */
-    protected static void planTasks(String fileName) {
+    private static void planTasks(String fileName) {
         //основное хранилище для отношения зависимости
-        HashSet<DataCont> allData=new HashSet<>();
+        HashSet<TaskContainer> allTasks = new HashSet<>();
         //хранилище для главных задач
-        HashSet<Integer> keys=new HashSet<>();
+        HashSet<Integer> mainTasks = new HashSet<>();
         //хранилище для зависимых задач
-        HashSet<Integer> vals = new HashSet<>();
-        try (FileReader dataInput = new FileReader(fileName)){
-            BufferedReader reader = new BufferedReader(dataInput);
+        HashSet<Integer> dependTasks = new HashSet<>();
+        try (FileReader fileReader = new FileReader(fileName)){
+            BufferedReader reader = new BufferedReader(fileReader);
             String line=reader.readLine();
-            //long k=0;
-            while(line!=null) {
+            while(line != null) {
                 //проходимся по файлу, запоминаем все отношения, а заодно разделяем задачи на главные и зависимые
-                DataCont singleOne= new Planner.DataCont(line.split(" "));
-                keys.add(singleOne.takeKey());
-                vals.add(singleOne.takeVal());
-                allData.add(singleOne);
-                line=reader.readLine();
-                //System.out.println((k++)+"");
+                TaskContainer singleTask = new TaskContainer(line.split(" "));
+                mainTasks.add(singleTask.getMainTask());
+                dependTasks.add(singleTask.getDepTask());
+                allTasks.add(singleTask);
+                line = reader.readLine();
             }
+            System.out.println("File's read.");
         }
-        catch(IOException ex) {
-            System.out.println(ex.getMessage());
+        catch(IOException e) {
+            e.printStackTrace();
         }
-        //и тут начинается магия с множествами
-        HashSet<Integer> nextKeys;
-        System.out.println("File's read.");
-        if (allData.isEmpty()) return;
+        //Подаем данные на обработку и вывод
+        processTasks(mainTasks,dependTasks,allTasks);
+
+    }
+    /**
+     * Берет на себя обработку и одновременный вывод данных.
+     * @param mains - подготовленное множество главных задач
+     * @param dependants - подготовленное множество зависимых задач
+     * @param tasksPairs - зависимости задач
+     */
+    private static void processTasks(HashSet<Integer> mains, HashSet<Integer> dependants, HashSet<TaskContainer> tasksPairs){
+        HashSet<Integer> newMainTasks;
+        if (tasksPairs.isEmpty()) return;
         try(FileWriter outputData = new FileWriter("outputData.txt", false))
         {
-            //System.out.println("First keys: "+keys.toString());
-            //int l=0;
             do {
-                nextKeys= new HashSet<>();
+                newMainTasks = new HashSet<>();
                 //System.out.println("Values: "+valList.toString());
                 //вычитаем множество зависимых из множества главных, получая независимые задачи
-                keys.removeAll(vals);
-                vals=new HashSet<>();
-                outputData.write(keys.toString());
+                mains.removeAll(dependants);
+                dependants = new HashSet<>();
+                outputData.write(mains.toString());
                 outputData.append('\n');
-                Iterator<DataCont> it = allData.iterator();
+                Iterator<TaskContainer> it = tasksPairs.iterator();
                 while(it.hasNext()){
                     //в цикле удаляем из множества отношений те, где главная задача независима, а зависимые от них
                     //помещаем в новое множество главных, чтобы не потерять
                     //заодно снова заполняем множество зависымых задач
-                    DataCont singleOne=it.next();
-                    if(keys.contains(singleOne.takeKey())){
-                        nextKeys.add(singleOne.takeVal());
+                    TaskContainer singleOne = it.next();
+                    if(mains.contains(singleOne.getMainTask())){
+                        newMainTasks.add(singleOne.getDepTask());
                         it.remove();
-                    }
-                    else{
-                        nextKeys.add(singleOne.takeKey());
-                        vals.add(singleOne.takeVal());
+                    } else {
+                        newMainTasks.add(singleOne.getMainTask());
+                        dependants.add(singleOne.getDepTask());
                     }
                 }
-                //выводим независсимые задачи этой итерации
-                keys=nextKeys;
-                //if((l%5)==0)System.out.println(l+", new keys: "+keys.toString());
-                //l++;
-            }while(!keys.isEmpty());
+                //выводим независимые задачи этой итерации
+                mains = newMainTasks;
+            }while(!mains.isEmpty());
             outputData.close();
         }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
+        catch(IOException e){
+            e.printStackTrace();
         }
     }
     /**
      * Встроенный класс для хранения пары главной-зависимой задач.
      */
-    protected static class DataCont{
+    protected static class TaskContainer{
         /** Пара из двух номеров задач */
-        protected Integer[] keyVal=new Integer[2];
+        private final Integer[] tasksPair = new Integer[2];
         /**
-         * Функция получения первого значения поля {@link DataCont#keyVal}
+         * Функция получения первого значения поля {@link TaskContainer#tasksPair}
          * @return возвращает номер главной задачи
          */
-        public Integer takeKey() {
-            return keyVal[0];
+        public Integer getMainTask() {
+            return tasksPair[0];
         }
-
         /**
-         * Функция получения второго значения поля {@link DataCont#keyVal}
+         * Функция получения второго значения поля {@link TaskContainer#tasksPair}
          * @return возвращает номер зависимой задачи
          */
-        public Integer takeVal(){
-            return keyVal[1];
+        public Integer getDepTask(){
+            return tasksPair[1];
         }
         /**
-         * Конструктор, вносящий в {@link DataCont#keyVal} значения из массива из двух строк.
+         * Конструктор, вносящий в {@link TaskContainer#tasksPair} значения из массива из двух строк.
          * @param s - массив из двух кнвертируемых в Integer строк, проверок не производится
          */
-        public DataCont(String[] s){
-            if (s.length==2) {
-                keyVal[0]=Integer.valueOf(s[0]);
-                keyVal[1]=Integer.valueOf(s[1]);
+        public TaskContainer(String[] s){
+            if (s.length == 2) {
+                tasksPair[0] = Integer.valueOf(s[0]);
+                tasksPair[1] = Integer.valueOf(s[1]);
             }
         }
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
-            DataCont dataCont = (DataCont) o;
-            return Arrays.equals(keyVal, dataCont.keyVal);
+            TaskContainer taskContainer = (TaskContainer) o;
+            return Arrays.equals(tasksPair, taskContainer.tasksPair);
         }
         @Override
         public int hashCode() {
-            return Arrays.hashCode(keyVal);
+            return Arrays.hashCode(tasksPair);
         }
     }
 }
